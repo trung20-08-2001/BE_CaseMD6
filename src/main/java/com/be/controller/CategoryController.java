a8866e15c570dda86a68d2ec21b3753eb6c16b38
Trung <nguyenvantrung200819dtd@gmail.com>
2023-08-29 16:15:18 +0700

    checkout test login

---
+++ src/main/java/com/be/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.be.controller;
+
+import com.be.model.Category;
+import com.be.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    ICategoryService categoryService;
+    @GetMapping("/findAll")
+    public List<Category> findAll(){
+        return categoryService.getAll();
+    }
+}
---
+++ src/main/java/com/be/service/ICategoryService.java
@@ -0,0 +1,9 @@
+package com.be.service;
+
+import com.be.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    List<Category> getAll();
+}
