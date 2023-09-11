06606976456262b96e0669d27c2b83d564889030
dinhvanson <dinhsonmta55@gmail.com>
2023-09-11 11:27:39 +0700

    fix FeedbackServiceImpl

--- src/main/java/com/be/service/impl/FeedBackServiceImpl.java
+++ src/main/java/com/be/service/impl/FeedBackServiceImpl.java
@@ -1,17 +1,22 @@
 package com.be.service.impl;
 
 import com.be.model.Feedback;
+import com.be.model.House;
+import com.be.model.Status;
 import com.be.repository.IFeedbackRepository;
+import com.be.repository.IHouseRepository;
 import com.be.service.IFeedBackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 @Service
-class FeedBackServiceImpl implements IFeedBackService {
+public class FeedbackServiceImpl implements IFeedBackService {
     @Autowired
     IFeedbackRepository iFeedbackRepository;
+    @Autowired
+    IHouseRepository iHouseRepository;
+
 
     @Override
     public void saveFeedback(Feedback feedback) {
@@ -25,8 +30,40 @@ class FeedBackServiceImpl implements IFeedBackService {
 
     @Override
     public Feedback findFeedbackByHouseAndUser(int idUser, int idHouse) {
-        List<Feedback> feedbacks= iFeedbackRepository.getFeedbackByHouseAndUser(idHouse,idUser).orElse(null);
+        List<Feedback> feedbacks = iFeedbackRepository.getFeedbackByHouseAndUser(idHouse, idUser).orElse(null);
         if (feedbacks == null || feedbacks.isEmpty()) return null;
         else return feedbacks.get(0);
+    }
+
+
+    @Override
+    public List<Feedback> getAllByHouse_Id(int houseId) {
+        House house = iHouseRepository.findById(houseId);
+        if (house != null) {
+            return iFeedbackRepository.getAllByHouse_Id(houseId);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Feedback updateFeedback(int feedbackId) {
+        Feedback feedback = iFeedbackRepository.findById(feedbackId).get();
+        if (feedback != null) {
+            Status status = new Status(3, "BLOCKED");
+            feedback.setStatus(status);
+            return iFeedbackRepository.save(feedback);
+        }
+        return null;
+    }
+
+    @Override
+    public List<Feedback> getAllByStar(int houseId, int start) {
+        House house = iHouseRepository.findById(houseId);
+        if (house != null) {
+            return iFeedbackRepository.getAllFeedbackByStar(houseId, start);
+        } else {
+            return null;
+        }
     }
 }
